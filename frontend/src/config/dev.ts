cc2a0a835e29a8f8d5a816f7ebd9c2ba727004e8
aleenareh <air.aleena.rehman@gmail.com>
2023-02-25 11:42:58 -0600

    fixed dev.ts to remove signature

--- frontend/src/config/dev.ts
+++ frontend/src/config/dev.ts
@@ -6,7 +6,6 @@ export const devConfig = {
   webEndpoint: 'zoom.us',
   topic: "a",
   signature: "",
-  // signature: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoieXlRS1o2d1NpZURHYzlEVEkxN3d3eHJaclBocmdack9qYmlBIiwidHBjIjoiYSIsInJvbGVfdHlwZSI6MSwidmVyc2lvbiI6MSwiaWF0IjoxNjc3MzQ0NDY0LCJleHAiOjE2NzczNTE2NjR9.FyRk2em3ta-lgGf1BSFycBTOXQqsrstSSaOerPqUPcI'",
   userName: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`,
   password: ""
 };
